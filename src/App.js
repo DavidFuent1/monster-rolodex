2a5decf80d0809172668daedffb4d371b94c0563
David Fuentes <D.gabefuentes@gmail.com>
2020-10-03 14:44:28 -0700

    Adds fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -7,19 +6,22 @@ class App extends React.Component {
     super();
 
     this.state = {
-      string: 'Hiiiii David',
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hiii kaykay' })}>
-            Say hi to Kaykay instead!
-          </button>
-        </header>
+        {this.state.monsters.map((monster, i) => (
+          <h1 key={i}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
