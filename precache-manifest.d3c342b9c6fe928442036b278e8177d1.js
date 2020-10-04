2eeaf3497807a1651b242c7871d6ef23e91d6087
David Fuentes <D.gabefuentes@gmail.com>
2020-10-03 22:13:10 -0700

    Updates

---
+++ precache-manifest.d3c342b9c6fe928442036b278e8177d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a07410315ad72d25e4c4ef580de21d",
+    "url": "/www.monster-dex.com/index.html"
+  },
+  {
+    "revision": "103e206991e4f22c10b4",
+    "url": "/www.monster-dex.com/static/css/main.2c6f61f1.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/www.monster-dex.com/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/www.monster-dex.com/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103e206991e4f22c10b4",
+    "url": "/www.monster-dex.com/static/js/main.230f97df.chunk.js"
+  },
+  {
+    "revision": "6453ec18f86314f8f6dc",
+    "url": "/www.monster-dex.com/static/js/runtime-main.cf4dc9b2.js"
+  }
+]);
