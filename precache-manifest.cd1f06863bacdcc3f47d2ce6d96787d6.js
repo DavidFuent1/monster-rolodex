81736a1d0016f6b441828097fd30187d99b12433
David Fuentes <D.gabefuentes@gmail.com>
2020-10-03 21:34:07 -0700

    Updates

---
+++ precache-manifest.cd1f06863bacdcc3f47d2ce6d96787d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a37268d6caf1d09e39672ae5e74974",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "575f2a43ec6b9103e17f",
+    "url": "/monsters-rolodex/static/css/main.2c6f61f1.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575f2a43ec6b9103e17f",
+    "url": "/monsters-rolodex/static/js/main.7f1d1326.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
